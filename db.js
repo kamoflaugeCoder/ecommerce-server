4e8da99544be8c2d928b515078a66da20d3e3dce
Nate Hill <nate.alan.hill@gmail.com>
2021-03-06 14:23:49 -0600

    jk

--- db.js
+++ db.js
@@ -3,12 +3,12 @@ require("dotenv").config()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    // dialectOptions: {
-    //     ssl: {
-    //         require: true,
-    //         rejectUnauthorized: false
-    //     }
-    //     }
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+        }
 })
 
 sequelize.authenticate().then(
