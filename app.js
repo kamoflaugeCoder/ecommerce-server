1ef9a29a5b0c22702a1fe44cf8841066f2542732
Nate Hill <nate.alan.hill@gmail.com>
2021-03-06 14:21:48 -0600

    fixed listen

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.use('/user', user)
 app.use('/beer', beer)
 
 
-applicationCache.addEventListener(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`server is listening on port ${process.env.PORT}`)
 })
--- db.js
+++ db.js
@@ -1,17 +1,14 @@
 const Sequelize = require('sequelize');
 require("dotenv").config()
 
-
-
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-        }
+    // dialectOptions: {
+    //     ssl: {
+    //         require: true,
+    //         rejectUnauthorized: false
+    //     }
+    //     }
 })
 
 sequelize.authenticate().then(
