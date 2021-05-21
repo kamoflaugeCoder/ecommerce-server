b7cba452d3825d7e6d0cc818db64f4fd8968fac1
tjcrowell <mrcrowell79@gmail.com>
2021-05-20 22:40:36 -0400

    Final Project

--- db.js
+++ db.js
@@ -2,35 +2,40 @@
 const Sequelize = require('sequelize');
 
 //2                     //3           //4         /5          //6
-const sequelize = new Sequelize('ecommerce', 'postgres', 'password', {
-    host: 'localhost',
-    dialect: 'postgres'
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+	dialect        : 'postgres',
+	dialectOptions : !process.env.DATABASE_URL.includes('localhost')
+		? {
+				ssl : {
+					require            : true,
+					rejectUnauthorized : false // <<<<<<< YOU NEED THIS TO FIX UNHANDLED REJECTION
+				}
+			}
+		: {}
 });
 
 //9            //10        //11
 sequelize.authenticate().then(() => {
-        console.log('Ecommerce server connected');
-    }),
+	console.log('Ecommerce server connected');
+}),
+	function(err) {
+		console.log(err);
+	};
 
-    function (err){
-            console.log(err)
-    }
+User = sequelize.import('./models/user');
+Review = sequelize.import('./models/review');
+Product = sequelize.import('./models/product');
+Cart = sequelize.import('./models/cart');
 
-    User = sequelize.import('./models/user');
-    Review = sequelize.import('./models/review');
-    Product = sequelize.import('./models/product');
-    Cart = sequelize.import('./models/cart');
-    
+Review.belongsTo(Product);
+Product.hasMany(Review);
 
+User.hasMany(Product);
+Product.belongsTo(User);
 
-    Review.belongsTo(Product);
-    Product.hasMany(Review);
-
-    User.hasMany(Product);
-    Product.belongsTo(User);
-
-    // .catch(err => {
-    //     console.error('Unable to connect to the database:', err);
-    // });
+// .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+// });
 //14
 module.exports = sequelize;
